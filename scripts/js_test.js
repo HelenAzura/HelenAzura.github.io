6a4093f106885fd7a1352bd7ff8ba7024fed6814
HelenAzura <52606609+HelenAzura@users.noreply.github.com>
2020-08-24 18:05:40 +0300

    Add files via upload

--- scripts/js_test.js
+++ scripts/js_test.js
@@ -1,52 +1,56 @@
-			function myFunction1 () {
-				if(document.getElementById("button1").innerText == 'Свернуть'){
-          document.getElementById("button1").innerText = 'Развернуть'}
-        else{document.getElementById("button1").innerText = 'Свернуть'};
+function myFunction1 () {
+	if(document.getElementById("button1").innerText == 'Свернуть'){
+       document.getElementById("button1").innerText = 'Развернуть'}
+    else{document.getElementById("button1").innerText = 'Свернуть'};
+    
+	document.getElementById("demo1").hidden = !document.getElementById("demo1").hidden;
+}
         
-				document.getElementById("demo1").hidden = !document.getElementById("demo1").hidden;
-				}
+function myFunction2 () {
+	if(document.getElementById("button2").innerText == 'Свернуть'){
+       document.getElementById("button2").innerText = 'Развернуть'}
+    else{document.getElementById("button2").innerText = 'Свернуть'};
         
-			function myFunction2 () {
-				if(document.getElementById("button2").innerText == 'Свернуть'){
-          document.getElementById("button2").innerText = 'Развернуть'}
-        else{document.getElementById("button2").innerText = 'Свернуть'};
+	document.getElementById("demo2").hidden = !document.getElementById("demo2").hidden;
+}
         
-				document.getElementById("demo2").hidden = !document.getElementById("demo2").hidden;
-				}
-        
-			function printForm(e){
-				var keyBox = document.print_text.print_text_box;
-				var val = keyBox.value;
-				var printBlock = document.getElementById("printBlock");
-				var pElement = document.createElement("p");
-				pElement.textContent = val;
-				printBlock.appendChild(pElement);
-			}
+function printForm(e){
+	let keyBox = document.getElementById("print_text_box");
+	let val = keyBox.value;
+	let printBlock = document.getElementById("printBlock");
+	let pElement = document.createElement("p");
+	
+	pElement.textContent = val;
+	printBlock.appendChild(pElement);
+}
       
-			function clear1() {
-				document.getElementById('printBlock').innerText = null;
-			}
+function clear1() {
+	document.getElementById('printBlock').innerText = null;
+}
       
-			var printButton = document.print_text.print_tx,
-      clearButton = document.print_text.clear_tx;
-			printButton.addEventListener("click", printForm);
-			clearButton.addEventListener("click", clear1);
+let printButton = document.getElementById('print_tx'),
+    clearButton = document.getElementById('clear_tx');
+	
+printButton.addEventListener("click", printForm);
+clearButton.addEventListener("click", clear1);
       
-			function addOption(){
-				var text = Form_students.textInput.value;
-				var value = Form_students.valueInput.value;
-				var newOption = new Option(text, value);
-				studentsSelect.options[studentsSelect.options.length]=newOption;
-			}
+function addOption(){
+	let text = document.getElementById('textInput').value;
+	let value = document.getElementById('valueInput').value;
+	let newOption = new Option(text, value);
+	
+	studentsSelect.options[studentsSelect.options.length]=newOption;
+}
       
-			function removeOption(){
-				var selectedIndex = studentsSelect.options.selectedIndex;
-				studentsSelect.options[selectedIndex] = null;
-			}
+function removeOption(){
+	let selectedIndex = studentsSelect.options.selectedIndex;
+	
+	studentsSelect.options[selectedIndex] = null;
+}
       
-			var addButton = Form_students.addButton,
-			removeButton = Form_students.removeButton, 
-			studentsSelect = Form_students.students;
+let addButton = Form_students.addButton,
+	removeButton = Form_students.removeButton, 
+	studentsSelect = Form_students.students;
       
-			addButton.addEventListener("click", addOption);
-			removeButton.addEventListener("click", removeOption);
+addButton.addEventListener("click", addOption);
+removeButton.addEventListener("click", removeOption);
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
-let page = document.querySelector('.page');
-let themeButton = document.querySelector('.theme-button');
-page.classList.add('dark-theme');
-themeButton.onclick = function() {
+let page = document.getElementById("page");
+let themeButton = document.getElementById("theme-button");
+page.classList.add('light-theme');
+themeButton.addEventListener("click", theme);
+function theme() {
 	page.classList.toggle('dark-theme');
 	}
