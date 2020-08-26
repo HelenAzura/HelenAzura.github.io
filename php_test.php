cc14c8fb227eda62db55245b1a4a89f3b6651adf
HelenAzura <52606609+HelenAzura@users.noreply.github.com>
2020-08-26 16:35:28 +0300

    Delete php_test.php

--- php_test.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$favorite_color = "зеленый";
-$favorite_number = 42;
-$welcome_text = "Привет, мой любимый цвет - " . $favorite_color . ", а любимое число - " . $favorite_number".";
-print($welcome_text);
