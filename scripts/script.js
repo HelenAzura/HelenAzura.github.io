cc2c27af5c95a0b042cc9f592c9891b76bbfe35b
HelenAzura <52606609+HelenAzura@users.noreply.github.com>
2020-09-03 14:13:44 +0300

    Delete script.js

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-let page = document.getElementById("page");
-let themeButton = document.getElementById("theme-button");
-page.classList.add('light-theme');
-themeButton.addEventListener("click", theme);
-function theme() {
-	page.classList.toggle('dark-theme');
-	}
