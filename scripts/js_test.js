2edbebd3dffcb4ae0166feed67dca0f39e8bb493
HelenAzura <52606609+HelenAzura@users.noreply.github.com>
2023-06-12 15:49:14 +0300

    Update js_test.js

--- scripts/js_test.js
+++ scripts/js_test.js
@@ -101,3 +101,17 @@ let studentsSelect = Form_students.students;
  
 printPicButton.addEventListener("click", addOption);
 removePicButton.addEventListener("click", removeOption);
+
+
+
+
+ function ready() {
+    alert('DOM готов');
+
+    // изображение ещё не загружено (если не было закешировано), так что размер будет 0x0
+    alert(`Размер изображения: ${img.offsetWidth}x${img.offsetHeight}`);
+  }
+
+
+
+document.addEventListener("DOMContentLoaded", ready);
